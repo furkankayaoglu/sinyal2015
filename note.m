16bbd7071842bb226ab722fbeae8f3d87fc5bd9b
SwordArtOnlineProject <leqendpasha@gmail.com>
2015-12-28 19:03:11 +0200

    update loading %99.99

--- note.m
+++ note.m
@@ -1,5 +1,9 @@
-function [xx,tt] = note(A,omega,phi,dur)
-ff=omega/2*pi
-tt = 0:1/(20*ff):dur; 
-xx = A*sin(2*pi*ff*tt+(phi));
-plot(tt,xx)
+function [x,t]=note(frekans,vurus) 
+Fs=8192; %ornekleme frekansı degeri
+t=0:1/Fs:((0.25-(1/Fs))/2); %t aralıgı
+z=0; %zarf matrisinin ilk degeri
+   x=sin(2*pi*frekans*t); %sinyal x e aktarildi
+if length(x)==length(z)   %length karsilastirilmasi
+       z=[linspace(0,1.5,Fs*vurus*(2/8)),linspace(1.5,1,Fs*vurus*(1/8)),linspace(1,1,Fs*vurus*(4/8)),linspace(1,0,Fs*vurus*(1/8))];%zarf değerlerinin matrisi
+end
+end
