a3e3d60d2093617855ebf8ce7fc9e157bc8c2078
SwordArtOnlineProject <leqendpasha@gmail.com>
2016-01-05 13:20:06 +0200

    last update

--- note.m
+++ note.m
@@ -1,9 +1,20 @@
-function [x,t]=note(frekans,vurus) 
-Fs=8192; %ornekleme frekansı degeri
-t=0:1/Fs:((0.25-(1/Fs))/2); %t aralıgı
-z=0; %zarf matrisinin ilk degeri
-   x=sin(2*pi*frekans*t); %sinyal x e aktarildi
-if length(x)==length(z)   %length karsilastirilmasi
-       z=[linspace(0,1.5,Fs*vurus*(2/8)),linspace(1.5,1,Fs*vurus*(1/8)),linspace(1,1,Fs*vurus*(4/8)),linspace(1,0,Fs*vurus*(1/8))];%zarf değerlerinin matrisi
+%%harmonik dizi tanimlamasi ve frekans orneklemesi
+function [xx,t]=note(ff,olcu) 
+fs=8192; %frekansin orneklenmesi
+harmo_x=0;
+harmonik={1,0.8,0.4,0.1}; %harmonik dizisi tanımlanması
+t=0:1/olcu:olcu-(1/fs); %t araliğinin hesaplanması.
+%%Her harmonik degeri icin uzerine toplayip ekleme
+for h=1:length(harmonik)%Her harmoniğin hesaplanması için kurduğum döngü
+   xx=harmonik{h}*sin(2*pi*ff*t); %Harmonik hesaplama
+   harmo_x=harmo_x+x;% Her değer için toplayıp üzerine ekleme
+
 end
+%%sinyalin zarflanmasi
+   z1=linspace(0,1.5,length(t)/4);
+   z2=linspace(1.5,1,length(t)/8);
+   z3=linspace(1,1,length(t)/2);
+   z4=linspace(1,0,length(t)/8);
+   zarf=[z1 z2 z3 z4];
+   xx=topla.*zarf; %Harmoniklendikten sonra sinyalin zarflanması
 end
