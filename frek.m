48778d7186578cb4b8bca9e9464628fb7dd813cd
SwordArtOnlineProject <leqendpasha@gmail.com>
2016-01-05 13:16:41 +0200

    last update

--- frek.m
+++ frek.m
@@ -1,70 +1,25 @@
-function [s] = frek(N,O)
-s1='N';
-s2='C(Do)';
-s3='C#';
-s4='D(Re)';
-s5='Eb';
-s6='E(Mi)';
-s7='F(Fa)';
-s8='F#';
-s9='G(Sol)';
-s10='G#';
-s11='A(La)';
-s12='Bb';
-s13='B(Si)';
-s14='Sus';
-if(strcmp(N,s2))
-    D=1;
+function ff=frek(note,octave)  
+notelar={'Do','Dod','Re','Mib','Mi','Fa','Fad','Sol','Sold','La','Sib','Si','Sus'}; %notalarin dizi icine atilmasi
+flag=16.35;  %do notesının 0.oktavdaki degerine atanmasi
+if nargin<2 %argümanların sayısı 2 den küçük ise
+     octave=4; %oktav 4
 end
-if(strcmp(N,s3))
-    D=2;
-end
-if(strcmp(N,s4))
-    D=3;
-end
-if(strcmp(N,s5))
-    D=4;
-end
-if(strcmp(N,s6))
-    D=5;
-end
-if(strcmp(N,s7))
-    D=6;
-end
-if(strcmp(N,s8))
-    D=7;
-end
-if(strcmp(N,s9))
-    D=8;
-end
-if(strcmp(N,s10))
-    D=9;
-end
-if(strcmp(N,s11))
-    D=10;
-end
-if(strcmp(N,s12))
-    D=11;
-end
-if(strcmp(N,s13))
-    D=12;
-end
-if(strcmp(N,s14))
-    F=0;
-end
-if(D==1)
-F=16.35
-else
-    F=16.35
-    for(j=1:D-1)
-        F=F*1.05933;
+
+       for i=1:length(notelar) %aldığımız note ile dizinin içindeki hangi notenın eşit olduğunu anlamk için döngü kurdum.
+           
+              if strcmp(note,notelar{i})%note ve dizinin i. elemanı bir birine eşit ise
+                  ff=2^octave*(2^((i-1)/12)*flag);%frekansin hesaplanmasi
+
+                  if strcmp(note,notelar{13})%Dur susa esitse
+                      ff=0; %frekansın 0 a esitlenmesi
+         
+              
+
+           
+       end
+       
     end
+     
 end
-if(O==0) 
-F2=1;
-F=F*F2;
-else 
-    for(i=1:O)
-        F=F*2;
-    end
+
 end
